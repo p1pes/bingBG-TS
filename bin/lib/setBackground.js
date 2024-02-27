30b4395c89be78a740d3a0b4a529bc7ed1417e33
Tim Piper <tpiper@cintra.com>
2024-02-27 14:51:58 +0000

    deploy script added, accepts random as param

--- bin/lib/setBackground.js
+++ bin/lib/setBackground.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -18,45 +41,73 @@ const fs_1 = __importDefault(require("fs"));
 const os_1 = __importDefault(require("os"));
 const setWallpaper_1 = require("./setWallpaper");
 const utils_1 = require("./utils");
+const date_fns_1 = require("date-fns");
+const path = __importStar(require("path"));
 const urlprefix = "https://www.bing.com";
 const url = `${urlprefix}/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=en-UK`;
 const settings = { method: "get" };
 const homeDirectory = os_1.default.homedir();
-const file = `${homeDirectory}/Pictures/bingBG/`;
-function main(days) {
+const imgDir = `${homeDirectory}/Pictures/bingBG/`;
+function main(type) {
     return __awaiter(this, void 0, void 0, function* () {
-        // Check if the string is a valid number or less than 8. if it is, set to 0
-        const imgNum = !isNaN(parseFloat(days)) ? parseFloat(days) < 8 ? parseFloat(days) : 0 : 0;
+        //just check if need to download any new images.    
         const json = yield getJson();
         for (let i = 0; i < json.images.length; i++) {
             let imageUrl = urlprefix + json.images[i].url;
-            let filename = file + (0, utils_1.camelCase)(json.images[i].title) + ".jpeg";
+            let filename = imgDir + (0, utils_1.camelCase)(json.images[i].title) + ".jpeg";
             yield downloadImage(imageUrl, filename);
         }
-        //set latest as background
-        yield (0, setWallpaper_1.setWallpaper)(file + (0, utils_1.camelCase)(json.images[imgNum !== null && imgNum !== void 0 ? imgNum : 0].title) + ".jpeg");
+        yield updateWallpaper(type !== null && type !== void 0 ? type : "0");
     });
 }
 exports.main = main;
+function updateWallpaper(action) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const json = yield getJson();
+        //default today
+        let filename = (0, utils_1.camelCase)(json.images[0].title) + ".jpeg";
+        //if number set background to number from last 8 days.
+        if (!isNaN(parseFloat(action))) {
+            filename = (0, utils_1.camelCase)(json.images[parseFloat(action) < 8 ? parseFloat(action) : 0].title) + ".jpeg";
+            (0, setWallpaper_1.setWallpaper)(imgDir + filename);
+            return;
+        }
+        switch (action.toLowerCase()) {
+            case "random":
+                filename = getRandomImage() + "";
+                break;
+        }
+        (0, setWallpaper_1.setWallpaper)(imgDir + filename);
+        // Check if the string is a valid number or less than 8. if it is, set to 0
+        //const imgNum = !isNaN(parseFloat(type)) ? parseFloat(type) < 8 ? parseFloat(type) : 0 : 0;
+        //await setWallpaper(file+camelCase(json.images[imgNum??0].title)+".jpeg");
+    });
+}
 function getJson() {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const response = yield axios_1.default.get(url, settings);
-            return response.data;
+        const cacheFileName = getCacheFileName();
+        if (!fs_1.default.existsSync(cacheFileName) || isStaleCache(cacheFileName)) {
+            console.log('Cached JSON file not found or stale. Downloading fresh copy...');
+            try {
+                const response = yield axios_1.default.get(url, settings);
+                fs_1.default.writeFileSync(cacheFileName, JSON.stringify(response.data));
+            }
+            catch (error) {
+                console.log(error);
+            }
         }
-        catch (error) {
-            console.log(error);
-        }
+        return JSON.parse(fs_1.default.readFileSync(cacheFileName, 'utf-8'));
     });
 }
 function downloadImage(imageUrl, filename) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             //make dir if not exist
-            fs_1.default.promises.mkdir(file, { recursive: true }).catch(console.error);
+            fs_1.default.promises.mkdir(imgDir, { recursive: true }).catch(console.error);
             const existsLocally = yield (0, utils_1.fileExistsLocally)(filename);
             if (existsLocally) {
                 //console.log('already downloaded', filename);
+                //already have this file, so just return
                 return;
             }
             const response = yield axios_1.default.get(imageUrl, { responseType: 'arraybuffer' });
@@ -67,4 +118,38 @@ function downloadImage(imageUrl, filename) {
             console.error('Error downloading image:', error);
         }
     });
+}
+// Function to generate a filename with the current date
+function getCacheFileName() {
+    const currentDate = (0, date_fns_1.format)(new Date(), 'yyyyMMdd');
+    return `data_${currentDate}.json`;
+}
+// Function to check if the cached file is stale (older than 24 hours)
+function isStaleCache(cacheFileName) {
+    const stats = fs_1.default.statSync(cacheFileName);
+    const currentTime = new Date().getTime();
+    const fileModificationTime = stats.mtime.getTime();
+    // Check if the file is older than 24 hours
+    return currentTime - fileModificationTime > 24 * 60 * 60 * 1000;
+}
+function getRandomImage() {
+    // Read the contents of the directory
+    try {
+        const files = fs_1.default.readdirSync(imgDir);
+        // Filter out directories, if any
+        const fileNames = files.filter(file => fs_1.default.statSync(path.join(imgDir, file)).isFile());
+        // Check if there are any files in the directory
+        if (fileNames.length === 0) {
+            console.error('No files found in the directory.');
+            return null;
+        }
+        // Generate a random index
+        const randomIndex = Math.floor(Math.random() * fileNames.length);
+        // Return the randomly selected filename
+        return fileNames[randomIndex];
+    }
+    catch (error) {
+        console.error('Error reading directory:', error);
+        return null;
+    }
 }
--- src/lib/setBackground.ts
+++ src/lib/setBackground.ts
@@ -3,43 +3,70 @@ import fs from 'fs';
 import os, { homedir } from 'os';
 import { setWallpaper } from './setWallpaper'
 import { camelCase, fileExistsLocally } from './utils';
+import { format } from 'date-fns';
+import * as path from 'path';
 
 const urlprefix = "https://www.bing.com";
 const url = `${urlprefix}/HPImageArchive.aspx?format=js&idx=0&n=8&mkt=en-UK`;
 const settings = { method: "get" }
 const homeDirectory = os.homedir();
-const file = `${homeDirectory}/Pictures/bingBG/`;
+const imgDir = `${homeDirectory}/Pictures/bingBG/`;
 
-export async function main(days: string){
-// Check if the string is a valid number or less than 8. if it is, set to 0
-const imgNum = !isNaN(parseFloat(days)) ? parseFloat(days) < 8 ? parseFloat(days) : 0 : 0;
-
+export async function main(type: string){
+    //just check if need to download any new images.    
     const json = await getJson();
     for (let i=0; i<json.images.length; i++) {
         let imageUrl = urlprefix + json.images[i].url;
-        let filename = file+camelCase(json.images[i].title)+".jpeg";
+        let filename = imgDir+camelCase(json.images[i].title)+".jpeg";
         await downloadImage(imageUrl, filename);
     }
-    //set latest as background
-    await setWallpaper(file+camelCase(json.images[imgNum??0].title)+".jpeg");    
+    await updateWallpaper(type??"0");
+}
+
+async function updateWallpaper(action: string){
+    const json = await getJson();
+    //default today
+    let filename : string = camelCase(json.images[0].title)+".jpeg";
+    //if number set background to number from last 8 days.
+    if (!isNaN(parseFloat(action))) {
+        filename = camelCase(json.images[parseFloat(action) < 8 ? parseFloat(action) : 0].title)+".jpeg";        
+        setWallpaper(imgDir+filename);
+        return;
+    }
+    switch (action.toLowerCase()) {
+        case "random" : 
+            filename = getRandomImage()+""; 
+            break;        
+    }
+    setWallpaper(imgDir+filename);
+
+// Check if the string is a valid number or less than 8. if it is, set to 0
+//const imgNum = !isNaN(parseFloat(type)) ? parseFloat(type) < 8 ? parseFloat(type) : 0 : 0;
+//await setWallpaper(file+camelCase(json.images[imgNum??0].title)+".jpeg");
 }
 
 async function getJson(){
-    try {
-        const response = await axios.get(url, settings);
-        return response.data;
-    } catch (error) {
-        console.log(error);
+    const cacheFileName = getCacheFileName();
+    if (!fs.existsSync(cacheFileName) || isStaleCache(cacheFileName)) {
+        console.log('Cached JSON file not found or stale. Downloading fresh copy...');
+        try {        
+            const response = await axios.get(url, settings);
+            fs.writeFileSync(cacheFileName, JSON.stringify(response.data));
+        } catch (error) {
+            console.log(error);
+        }
     }
+    return JSON.parse(fs.readFileSync(cacheFileName, 'utf-8'));
 }
 
 async function downloadImage(imageUrl: string, filename: string){
     try {
         //make dir if not exist
-        fs.promises.mkdir(file, { recursive: true }).catch(console.error);
+        fs.promises.mkdir(imgDir, { recursive: true }).catch(console.error);
         const existsLocally = await fileExistsLocally(filename);
         if (existsLocally) {
             //console.log('already downloaded', filename);
+            //already have this file, so just return
             return;
         }
         const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
@@ -50,4 +77,39 @@ async function downloadImage(imageUrl: string, filename: string){
     }
 }
 
+// Function to generate a filename with the current date
+function getCacheFileName(): string {
+    const currentDate = format(new Date(), 'yyyyMMdd');
+    return `data_${currentDate}.json`;
+}
 
+// Function to check if the cached file is stale (older than 24 hours)
+function isStaleCache(cacheFileName: string): boolean {
+    const stats = fs.statSync(cacheFileName);
+    const currentTime = new Date().getTime();
+    const fileModificationTime = stats.mtime.getTime();
+    // Check if the file is older than 24 hours
+    return currentTime - fileModificationTime > 24 * 60 * 60 * 1000;
+}
+
+
+function getRandomImage(): string | null {
+    // Read the contents of the directory
+    try {
+        const files = fs.readdirSync(imgDir);
+        // Filter out directories, if any
+        const fileNames = files.filter(file => fs.statSync(path.join(imgDir, file)).isFile());
+        // Check if there are any files in the directory
+        if (fileNames.length === 0) {
+            console.error('No files found in the directory.');
+            return null;
+        }
+        // Generate a random index
+        const randomIndex = Math.floor(Math.random() * fileNames.length);
+        // Return the randomly selected filename
+        return fileNames[randomIndex];
+    } catch (error) {
+        console.error('Error reading directory:', error);
+        return null;
+    }
+}
